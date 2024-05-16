3bf8d9c137d55061525d8fb2a8e1229cc8841e96
alex <ricardo_ardi@rocketmail.com>
2024-05-16 14:28:39 +0700

    add button

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+const seven = document.querySelector(".seven")
+const eight = document.querySelector(".eight")
+const nine = document.querySelector(".nine")
+const bagi = document.querySelector(".divide")
+const four = document.querySelector(".four")
+const five = document.querySelector(".five")
+const six = document.querySelector(".six")
+const kali = document.querySelector(".multiply")
+const one = document.querySelector(".one")
+const two = document.querySelector(".two")
+const three = document.querySelector(".three")
+const kurang = document.querySelector(".subtract")
+const zero = document.querySelector(".zero")
+const clear = document.querySelector(".clear")
+const tambah = document.querySelector(".add")
+const equal = document.querySelector(".equal")
+const display = document.querySelector(".display")
+const decimal = document.querySelector(".decimal")
+
+seven.addEventListener("click",()=>{
+    display.textContent+="7"
+})
+eight.addEventListener("click",()=>{
+    display.textContent+="8"
+})
+nine.addEventListener("click",()=>{
+    display.textContent+="9"
+})
+bagi.addEventListener("click",()=>{
+    display.textContent+="/"
+})
+four.addEventListener("click",()=>{
+    display.textContent+="4"
+})
+five.addEventListener("click",()=>{
+    display.textContent+="5"
+})
+six.addEventListener("click",()=>{
+    display.textContent+="6"
+})
+kali.addEventListener("click",()=>{
+    display.textContent+="x"
+})
+one.addEventListener("click",()=>{
+    display.textContent+="1"
+})
+two.addEventListener("click",()=>{
+    display.textContent+="2"
+})
+three.addEventListener("click",()=>{
+    display.textContent+="3"
+})
+kurang.addEventListener("click",()=>{
+    display.textContent+="-"
+})
+zero.addEventListener("click",()=>{
+    display.textContent+="0"
+})
+clear.addEventListener("click",()=>{
+    display.textContent=""
+})
+tambah.addEventListener("click",()=>{
+    display.textContent+="+"
+})
+equal.addEventListener("click",()=>{
+    display.textContent+="="
+})
+
+decimal.addEventListener("click",()=>{
+    display.textContent+="."
+})
+
 function add(a,b){
     return a+b;
 }
